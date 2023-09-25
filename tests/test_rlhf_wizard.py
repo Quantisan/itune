996609d9b959f9f23df709daebcf74e5e436bf04
Paul Lam <paul@quantisan.com>
2023-09-25 10:21:00 +0900

    add test_experiment

--- tests/test_rlhf_wizard.py
+++ tests/test_rlhf_wizard.py
@@ -1,5 +1,3 @@
-import pytest
-
 import rlhf_wizard as rlhf
 
 
@@ -7,3 +5,12 @@ def test_instantiate_model():
     rlhf_model = rlhf.Model()
     assert isinstance(rlhf_model, rlhf.Model)
 
+
+def test_experiment():
+    model = rlhf.Model()
+
+    @model.experiment(variations=[3])
+    def three(model) -> int:
+        return model.get_variation()
+
+    assert three(model) == 3
