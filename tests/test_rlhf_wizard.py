7ebbd53fe9a5c578d48b901a36b926537fea6a4c
Paul Lam <paul@quantisan.com>
2023-10-04 08:22:30 +0900

    add failure tests to parameter()

--- tests/test_rlhf_wizard.py
+++ tests/test_rlhf_wizard.py
@@ -12,5 +12,21 @@ def test_instantiate_model():
     assert isinstance(rlhf.model, rlhf.Model)
 
 
-def test_parameter():
-    assert rlhf.model.parameter(x=[3]) == 3
+def test_model_parameter():
+    result = rlhf.model.parameter(x=[1, 2, 3])
+    assert result in [1, 2, 3]
+
+
+def test_model_parameter_multiple_variations():
+    with pytest.raises(ValueError):
+        rlhf.model.parameter(variation=[1, 2, 3], variation2=[4, 5, 6])
+
+
+def test_model_parameter_empty_variation():
+    with pytest.raises(ValueError):
+        rlhf.model.parameter(variation=[])
+
+
+def test_model_parameter_non_list_variation():
+    with pytest.raises(ValueError):
+        rlhf.model.parameter(variation=1)
