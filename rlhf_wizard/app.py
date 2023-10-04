2730101139936bdfcbfd8a88975c2afd9a7ad98c
Paul Lam <paul@quantisan.com>
2023-10-04 14:08:44 +0900

    move _validate_variations as a self.* method

--- rlhf_wizard/app.py
+++ rlhf_wizard/app.py
@@ -2,24 +2,23 @@ import random
 from typing import Any
 
 
-def _validate_variations(kwargs):
-    if (
-        len(kwargs) != 1
-        or not isinstance(kwargs.get(list(kwargs.keys())[0]), list)
-        or not kwargs.get(list(kwargs.keys())[0])
-    ):
-        raise ValueError("Argument must have exactly one non-empty list value")
-
-
 class Model:
     _model: dict[str, list] = {}
     _current_selections: dict[str, Any] = {}
+
+    def _validate_parameter_variations(self, kwargs):
+        if (
+            len(kwargs) != 1
+            or not isinstance(kwargs.get(list(kwargs.keys())[0]), list)
+            or not kwargs.get(list(kwargs.keys())[0])
+        ):
+            raise ValueError("Argument must have exactly one non-empty list value")
 
     def _track_choice(self, key, choice):
         self._current_selections[key] = choice
 
     def parameter(self, **kwargs):
-        _validate_variations(kwargs)
+        self._validate_parameter_variations(kwargs)
 
         k, value_list = list(kwargs.items())[0]
         choice = random.choice(value_list)
