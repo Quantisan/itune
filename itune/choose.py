5c4163eac07fb4ea49e840d289bf0a7d13f55254
Paul Lam <paul@quantisan.com>
2023-10-11 08:07:55 +0900

    refactored MultiArmedBandit to hold a trail_counts state

--- itune/choose.py
+++ itune/choose.py
@@ -1,32 +1,59 @@
 import random
 
 
+def calculate_expected_reward(successes, failures):
+    if (successes + failures) > 0:
+        return successes / (successes + failures)
+    else:
+        return 0
+
+
 class MultiArmedBandit:
-    def __init__(self, successes, failures, epsilon=0.05):
-        self.successes = successes
-        self.failures = failures
+    def __init__(self, epsilon=0.05):
         self.epsilon = epsilon
+        # trial_counts should have a schema, or it could be a class
+        self.trial_counts = {}
+
+    def _arms(self, parameter):
+        return list(self.trial_counts[parameter]["successes"].keys())
+
+    def _successes(self, parameter, arm):
+        return self.trial_counts[parameter]["successes"][arm]
+
+    def _failures(self, parameter, arm):
+        return self.trial_counts[parameter]["failures"][arm]
 
     def _ensure_chosen_type(self, choice_str, value_list):
         return value_list[value_list.index(eval(choice_str))]
 
-    def _expected_reward(self, arm):
-        if (self.successes[arm] + self.failures[arm]) > 0:
-            return self.successes[arm] / (self.successes[arm] + self.failures[arm])
-        else:
-            return 0
+    def _seed_trial_counts(self, parameter, arms):
+        self.trial_counts[parameter] = {
+            "successes": {str(arm): 0 for arm in arms},
+            "failures": {str(arm): 0 for arm in arms},
+        }
 
-    def choose(self, value_list):
-        values = [self._expected_reward(arm) for arm in self.successes.keys()]
+    def choose(self, parameter, value_list):
+        if parameter not in self.trial_counts:
+            self._seed_trial_counts(parameter, value_list)
+
+        expected_rewards = [
+            calculate_expected_reward(
+                self._successes(parameter, arm), self._failures(parameter, arm)
+            )
+            for arm in self._arms(parameter)
+        ]
         # epsilon-greedy
         if random.random() > self.epsilon:
             return self._ensure_chosen_type(
-                list(self.successes.keys())[values.index(max(values))], value_list
+                list(self._arms(parameter))[
+                    expected_rewards.index(max(expected_rewards))
+                ],
+                value_list,
             )
         else:
             return (
                 self._ensure_chosen_type(
-                    random.choice(list(self.successes.keys())),
+                    random.choice(list(self._arms(parameter))),
                     value_list,
                 ),
             )
--- tests/test_choose.py
+++ tests/test_choose.py
@@ -2,12 +2,12 @@ from itune import MultiArmedBandit
 
 
 def test_mab_choose():
-    mab = MultiArmedBandit(successes={"1": 0, "2": 0}, failures={"1": 0, "2": 1})
-    choice = mab.choose([1, 2])
+    mab = MultiArmedBandit()
+    choice = mab.choose("x", [1, 2])
     assert choice in [1, 2]
 
 
 def test_ensure_chosen_type():
-    mab = MultiArmedBandit(successes={"1": 0, "2": 0}, failures={"1": 0, "2": 0})
+    mab = MultiArmedBandit()
     choice = mab._ensure_chosen_type("1", [1, 2])
     assert choice == 1
