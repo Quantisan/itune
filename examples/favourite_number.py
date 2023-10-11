fa0122d2beb6bec4a2f531aa30f645d52ef8a92c
Paul Lam <paul@quantisan.com>
2023-10-12 07:49:42 +0900

    copy edit fav_num.py

--- examples/favourite_number.py
+++ examples/favourite_number.py
@@ -5,10 +5,10 @@ itune = Tune(strategy=MultiArmedBandit())
 
 for _ in range(10):
     print(
-        f"Everyone's favourite number from 1 to {MAX_VALUE} (inclusive) is {itune.choose(fav_num=list(range(1,5+1)))}"
+        f"Your favourite number from 1 to {MAX_VALUE} (inclusive) is {itune.choose(fav_num=list(range(1,5+1)))}"
     )
 
-    user_input = input("Agree (y) / Disagree (n)?")
+    user_input = input("Yes (y) / No (n)?")
     ######################
     # reward function
     itune.register_outcome(user_input == "y")
