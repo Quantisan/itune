8572f8e026226238313c061ba9a8c27737bc3987
Paul Lam <paul@quantisan.com>
2023-10-11 09:57:14 +0900

    reordered test_choose.py tests

--- tests/test_choose.py
+++ tests/test_choose.py
@@ -1,13 +1,13 @@
 from itune import MultiArmedBandit
 
 
-def test_mab_choose():
-    mab = MultiArmedBandit()
-    choice = mab.choose("x", [1, 2])
-    assert choice in [1, 2]
-
-
 def test_ensure_chosen_type():
     mab = MultiArmedBandit()
     choice = mab._ensure_chosen_type("1", [1, 2])
     assert choice == 1
+
+
+def test_mab_choose():
+    mab = MultiArmedBandit()
+    choice = mab.choose("x", [1, 2])
+    assert choice in [1, 2]
